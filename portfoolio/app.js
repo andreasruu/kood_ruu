1d1b294ff040b3ceb99b72fbb768675746a8a563
Andreas Ruu <andreas.ruu@gmail.com>
2023-12-17 16:20:04 +0200

    portfoolio

---
+++ portfoolio/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log(`Server töötab aadressil http://localhost:${port}`);
+});
